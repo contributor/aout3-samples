95d6f38577e7c633e10732d92ffc522d80a5708d
contributor <foss.contributor@gmail.com>
2025-08-14 14:28:40 +0300

    fix deprecation warning
     Deprecation Warning:
    
      Option "testURL" was replaced by passing the URL via "testEnvironmentOptions.url".
    
      Please update your configuration.
    
      Configuration Documentation:
      https://jestjs.io/docs/configuration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  testURL: "http://localhost/",
+  testEnvironmentOptions: { url: "http://localhost/" },
   preset: "ts-jest",
   testEnvironment: "node",
   // testEnvironment: 'jest-environment-jsdom',
   testEnvironmentOptions: {
     "url": "http://localhost/"
-},
+  },
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
